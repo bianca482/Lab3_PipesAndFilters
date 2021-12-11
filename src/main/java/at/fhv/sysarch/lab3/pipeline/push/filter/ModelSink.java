f1e00654a11a6eb2474941492c5a47cccdadbad7
bianca482 <80405628+bianca482@users.noreply.github.com>
2021-12-11 08:36:38 +0100

    Added renderingMode for ModelSink

--- src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/ModelSink.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/ModelSink.java
@@ -8,7 +8,6 @@ import com.hackoeur.jglm.Vec2;
 import com.hackoeur.jglm.Vec4;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
 
 public class ModelSink implements Filter<Pair<Face, Color>> {
 
@@ -26,12 +25,26 @@ public class ModelSink implements Filter<Pair<Face, Color>> {
         Vec4 v2Trans = faceColorPair.fst().getV2();
         Vec4 v3Trans = faceColorPair.fst().getV3();
 
+        Vec2 v1Screen = v1Trans.toScreen();
+        Vec2 v2Screen = v2Trans.toScreen();
+        Vec2 v3Screen = v3Trans.toScreen();
+
         RenderingMode renderingMode = pd.getRenderingMode();
 
-        context.setStroke(faceColorPair.snd());
+        if (renderingMode.equals(RenderingMode.POINT)) {
+            context.setStroke(faceColorPair.snd());
 
-        context.strokeLine(v1Trans.getX(), v1Trans.getY(), v2Trans.getX(), v2Trans.getY());
-        context.strokeLine(v1Trans.getX(), v1Trans.getY() , v3Trans.getX(), v3Trans.getY());
-        context.strokeLine(v2Trans.getX(), v2Trans.getY() , v3Trans.getX(), v3Trans.getY());
+            context.strokeLine(v1Trans.getX(), v1Trans.getY(), v2Trans.getX(), v2Trans.getY());
+            context.strokeLine(v1Trans.getX(), v1Trans.getY() , v3Trans.getX(), v3Trans.getY());
+            context.strokeLine(v2Trans.getX(), v2Trans.getY() , v3Trans.getX(), v3Trans.getY());
+        } else if (renderingMode.equals(RenderingMode.WIREFRAME)) {
+            context.setStroke(faceColorPair.snd());
+
+            context.strokePolygon(new double[]{v1Screen.getX(), v2Screen.getX(), v3Screen.getX()}, new double[]{v1Screen.getY(), v2Screen.getY(), v3Screen.getY()}, 3);
+        } else if (renderingMode.equals(RenderingMode.FILLED)) {
+            context.setFill(faceColorPair.snd());
+
+            context.fillPolygon(new double[]{v1Screen.getX(), v2Screen.getX(), v3Screen.getX()}, new double[]{v1Screen.getY(), v2Screen.getY(), v3Screen.getY()}, 3);
+        }
    }
 }
