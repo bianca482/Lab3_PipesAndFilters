9ee45a4b6d7d4d4ca92d5bd06bf1db0bbba29e6f
bianca482 <80405628+bianca482@users.noreply.github.com>
2021-12-13 08:57:54 +0100

    Moved Coloring to Step 4

--- src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/ModelSink.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/ModelSink.java
@@ -37,7 +37,6 @@ public class ModelSink implements Filter<Pair<Face, Color>> {
             context.fillOval(v1Trans.getX(), v1Trans.getY(), 1, 1);
             context.fillOval(v2Trans.getX(), v2Trans.getY(), 1, 1);
             context.fillOval(v3Trans.getX(), v3Trans.getY(), 1, 1);
-
         } else if (renderingMode.equals(RenderingMode.WIREFRAME)) {
             context.setStroke(faceColorPair.snd());
 
--- src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/PerspectiveProjection.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/push/filter/PerspectiveProjection.java
@@ -2,30 +2,34 @@ package at.fhv.sysarch.lab3.pipeline.push.filter;
 
 import at.fhv.sysarch.lab3.obj.Face;
 import at.fhv.sysarch.lab3.pipeline.PipelineData;
+import at.fhv.sysarch.lab3.pipeline.data.Pair;
 import at.fhv.sysarch.lab3.pipeline.push.pipe.Pipe;
 import com.hackoeur.jglm.Mat4;
 import com.hackoeur.jglm.Vec4;
+import javafx.scene.paint.Color;
 
-public class PerspectiveProjection implements Filter<Face> {
+public class PerspectiveProjection implements Filter<Pair<Face, Color>> {
     private PipelineData pd;
-    private Pipe<Face> successor;
+    private Pipe<Pair<Face, Color>> successor;
 
     public PerspectiveProjection(PipelineData pd) {
         this.pd = pd;
     }
 
     @Override
-    public void write(Face input) {
+    public void write(Pair<Face, Color> input) {
         Mat4 projTransform = pd.getProjTransform();
 
-        Vec4 v1Trans = projTransform.multiply(input.getV1());
-        Vec4 v2Trans = projTransform.multiply(input.getV2());
-        Vec4 v3Trans = projTransform.multiply(input.getV3());
+        Vec4 v1Trans = projTransform.multiply(input.fst().getV1());
+        Vec4 v2Trans = projTransform.multiply(input.fst().getV2());
+        Vec4 v3Trans = projTransform.multiply(input.fst().getV3());
 
-        successor.write(new Face(v1Trans, v2Trans, v3Trans, input.getN1(), input.getN2(), input.getN3()));
+        Pair<Face, Color> newInput = new Pair<>(new Face(v1Trans, v2Trans, v3Trans, input.fst().getN1(), input.fst().getN2(), input.fst().getN3()), input.snd());
+
+        successor.write(newInput);
     }
 
-    public void setSuccessor(Pipe<Face> successor) {
+    public void setSuccessor(Pipe<Pair<Face, Color>> successor) {
         this.successor = successor;
     }
 }
